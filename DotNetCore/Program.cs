999f377b47d8bc3733a8bfc67f7d00bebdf6b500
OliverMengich <oliverkemei2000@gmail.com>
2019-10-23 18:13:48 +0300

    Live Share

---
+++ DotNetCore/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, We are using Visual Studio Live Share");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}");
+            
+
+        }
+    }
+}
